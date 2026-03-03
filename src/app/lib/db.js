1c02e7fc256a0789bb3a0f618e5d6864da11256d
Sunday Famous <sundayfamous25@yahoo.com>
2026-03-03 17:32:09 +0100

    kjkg

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -14,8 +14,8 @@ const sql = postgres(process.env.POSTGRES_URL, {
   ssl: process.env.NODE_ENV === "production" ? "require" : false,
   idle_timeout: 20,
   max_lifetime: 60 * 30,
-  max: 1,
-  connect_timeout: 10, // ✅ add this
+  max: 10,
+  // connect_timeout: 10, // ✅ add this
   prepare: false, 
 });
 export default sql;
