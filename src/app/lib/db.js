28f37b5498bbafd3cea5ac9454807906990f7782
Sunday Famous <sundayfamous25@yahoo.com>
2026-03-03 22:26:40 +0100

    1

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -2,20 +2,26 @@ import postgres from "postgres";
 
 
 
-// const sql = postgres(process.env.POSTGRES_URL, {
-//   ssl: process.env.NODE_ENV === "production" ? "require" : false,
-//   idle_timeout: 20,
-//   max: 1,
-// });
-
-
-
 const sql = postgres(process.env.POSTGRES_URL, {
-  ssl: process.env.NODE_ENV === "production" ? "require" : false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   idle_timeout: 20,
   max_lifetime: 60 * 30,
   max: 10,
-  // connect_timeout: 10, // ✅ add this
-  prepare: false, 
+  prepare: false,
 });
+
 export default sql;
+
+
+// const sql = postgres(process.env.POSTGRES_URL, {
+//   ssl: process.env.NODE_ENV === "production" ? "require" : false,
+//   idle_timeout: 20,
+//   max_lifetime: 60 * 30,
+//   max: 10,
+//   // connect_timeout: 10, // ✅ add this
+//   prepare: false, 
+// });
+// export default sql;
